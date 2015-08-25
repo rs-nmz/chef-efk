cfed0b9722bd21ffcf997ea99fec5e5922ed7e2f
yotsu1987 <fj3355dy@aa.jp.fujitsu.com>
2015-08-25 22:54:55 +0900

    Update default.rb

--- cookbooks/elasticsearch/recipes/default.rb
+++ cookbooks/elasticsearch/recipes/default.rb
@@ -8,25 +8,25 @@
 #
 
 filename1 = "elasticsearch-1.5.2.noarch.rpm"
-filename2 = "java-1.8.0-openjdk-1.8.0.25-1.b17.el6.x86_64.rpm"
+#filename2 = "java-1.8.0-openjdk-1.8.0.25-1.b17.el6.x86_64.rpm"
 
 cookbook_file "/tmp/#{filename1}" do
   source "#{filename1}"
 end
 
-cookbook_file "/tmp/#{filename2}" do
-  source "#{filename2}"
-end
+#cookbook_file "/tmp/#{filename2}" do
+#  source "#{filename2}"
+#end
 
 package "elasticsearch" do
   action :install
   source "/tmp/#{filename1}"
 end
 
-package "OpenJDK" do
-  action :install
-  source "/tmp/#{filename2}"
-end
+#package "OpenJDK" do
+#  action :install
+#  source "/tmp/#{filename2}"
+#end
 
 template "/etc/elasticsearch/elasticsearch.yml" do
   source "elasticsearch.yml.erb"
@@ -40,6 +40,8 @@ service "elasticsearch" do
   supports :status => true, :restart => true, :reload => true
   action [:enable, :start]
 end
+
+
 
 
 
