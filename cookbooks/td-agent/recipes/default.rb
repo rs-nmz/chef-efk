a1944b1145e3838bed00dfc73121a455beaca213
yotsu1987 <fj3355dy@aa.jp.fujitsu.com>
2015-08-25 22:47:35 +0900

    Update default.rb

--- cookbooks/td-agent/recipes/default.rb
+++ cookbooks/td-agent/recipes/default.rb
@@ -34,22 +34,26 @@ directory '/etc/td-agent/' do
   action :create
 end
 
-filename = "td-agent-2.2.0-0.x86_64.rpm"
+filename1 = "td-agent-2.2.0-0.x86_64.rpm"
+filename2 = "fluent-plugin-elasticsearch-0.9.0.gem"
+filename3 = "fluent-plugin-dstat-0.3.1.gem"
+filename4 = "fluent-plugin-map-0.0.5.gem"
 
-cookbook_file "/tmp/#{filename}" do
-  source "#{filename}"
+
+cookbook_file "/tmp/#{filename1}" do
+  source "#{filename1}"
 end
 
 
 package "td-agent" do
   action :install
-  source "/tmp/#{filename}"
+  source "/tmp/#{filename1}"
 end
 
 
 #package "td-agent" do
 #  action :upgrade
-#  source "/tmp/#{filename}"
+#  source "/tmp/#{filename1}"
 #end
 
 file '/var/log/td-agent/messages.log.pos' do
@@ -71,6 +75,49 @@ end
 
 template "/etc/td-agent/td-agent.conf" do
   mode "0644"
+  source "td-agent_first.conf.erb"
+  notifies :restart, "service[td-agent]"
+end
+
+service "td-agent" do
+  action [:enable, :start]
+  subscribes :restart, resources(:template => "/etc/td-agent/td-agent.conf")
+end
+
+#plugin install
+cookbook_file "/tmp/#{filename2}" do
+  source "#{filename2}"
+end
+
+cookbook_file "/tmp/#{filename3}" do
+  source "#{filename3}"
+end
+
+cookbook_file "/tmp/#{filename4}" do
+  source "#{filename4}"
+end
+
+gem_package "fluentd-elasticsearch" do
+  gem_binary "/usr/sbin/td-agent-gem"
+  action :install
+  source "/tmp/#{filename2}"
+end
+
+gem_package "fluentd-dstat" do
+  gem_binary "/usr/sbin/td-agent-gem"
+  action :install
+  source "/tmp/#{filename3}"
+end
+
+gem_package "fluentd-map" do
+  gem_binary "/usr/sbin/td-agent-gem"
+  action :install
+  source "/tmp/#{filename4}"
+end
+
+
+template "/etc/td-agent/td-agent.conf" do
+  mode "0644"
   source "td-agent.conf.erb"
   notifies :restart, "service[td-agent]"
 end
@@ -80,4 +127,6 @@ service "td-agent" do
   action [:enable, :start]
   subscribes :restart, resources(:template => "/etc/td-agent/td-agent.conf")
 end
+
+
 
